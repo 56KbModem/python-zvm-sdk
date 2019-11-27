8ecf76cbfea72d61b198d8e962803795d23435e1
56KbModem <nick-snel@hotmail.com>
2019-11-27 11:23:03 +0100

    added decode logic for bytestream returned by subprocess call

--- smtLayer/scanFCP.py
+++ smtLayer/scanFCP.py
@@ -1,6 +1,21 @@
 import subprocess # to call lsluns
 
 # --DUCK--
+
+# Decode the bytestream given to us by the
+# subprocess call to from ascii to unicode
+def concatenate(stream):
+	output = []
+	line = ""
+
+	for byte in stream.decode():
+		if byte != '\n':
+			line += byte
+		else:
+			output.append(line)
+			line = ""
+	return output
+
 # Create a new function to scan for free LUN's and
 # accompanying FCP devices.
 def scanFCP(rh):
@@ -26,11 +41,12 @@ def scanFCP(rh):
 			str(rh.results["overallRC"]))
 	else:
 		scan_ouput = subprocess.check_output(["lsluns"])
-		if len(scan_output) == 0:		# Cannot scan for LUN's; failure!
+		if not scan_output:	# Cannot scan for luns failure
 			rh.results["overallRC"] = 3	# Return non-zero return code (exit with failure)
 			rh.printSysLog("Failed to scan for LUN's on the SAN fabric: " +
 				str(rh.results["overallRC"]))
 		else:
+			scan_output = concatenate(scan_output) # decode output to unicode
 			for line in scan_output:
 				if "port" in line:
 					wwpns = line.split('0x')[1].strip(':')
